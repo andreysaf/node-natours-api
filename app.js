fe639cb7fd3354c3e23e79123b6b5f26366cf56c
Andrey <a.so777@live.ca>
2020-05-03 10:53:23 -0700

    Added tours endpoint

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
 
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'hello!'});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 });
 
 app.listen(port, () => {
