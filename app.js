6e5e4e04cffda07e6c397d8c2160e3958d6d8dd9
Andrey <a.so777@live.ca>
2020-05-03 11:42:35 -0700

    Added another entry point, reformatting

--- app.js
+++ app.js
@@ -1,22 +1,65 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const express = require('express');
+const express = require("express");
 const app = express();
+app.use(express.json());
 
 const port = 3001;
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (tour) {
     res.status(200).json({
-        status: 'success',
-        results: tours.length,
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      message: `Could not find tour with ID of ${id}`,
+    });
+  }
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
         data: {
-            tours
-        }
-    })
-});
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app.route("/api/v1/tours/:id").get(getTour);
 
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`)
-})
+  console.log(`Server listening on port: ${port}`);
+});
