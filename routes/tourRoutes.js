88b4100aa929f3fe4a61f33faf82cdc3dd7ebcb7
Andrey <a.so777@live.ca>
2020-06-07 09:58:08 -0700

    Implemented aliasing

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createTour, getAllTours, getTour, updateTour, deleteTour } = require('../controller/tourController');
+const { aliasTopTours, createTour, getAllTours, getTour, updateTour, deleteTour } = require('../controller/tourController');
 
 const router = express.Router();
+router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
