bfd9fe094497c379dee564a23498b206fae60adc
Andrey <a.so777@live.ca>
2020-06-10 17:39:04 -0700

    Implemented to get amount of tours per month

--- controller/tourController.js
+++ controller/tourController.js
@@ -137,3 +137,48 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = Number(req.params.year);
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: { startDates: { $gte: new Date(`${year}-01-01`), $lte: new Date(`${year}-12-31`) }}
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+          tours: { $push: '$name'}
+        }
+      }, 
+      {
+        $sort: {
+          numTours: -1
+        }
+      },
+      {
+        $addFields: { month: '$_id'}
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      }
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { aliasTopTours, getTourStats, createTour, getAllTours, getTour, updateTour, deleteTour } = require('../controller/tourController');
+const { aliasTopTours, getTourStats, getMonthlyPlan, createTour, getAllTours, getTour, updateTour, deleteTour } = require('../controller/tourController');
 
 const router = express.Router();
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
