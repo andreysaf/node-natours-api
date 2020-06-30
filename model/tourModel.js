130ae91e565af533bacebb5f802bcf536f5e597f
Andrey <a.so777@live.ca>
2020-06-29 19:33:34 -0700

    Implemented custom validator

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,82 +1,97 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name.'],
-    unique: true,
-    trim: true,
-    maxlength: [40, 'A tour name must have less or equal than 40 characters'],
-    minlength: [10, 'A tour name must have greater or equal than 10 characters'],
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name.'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [
+        10,
+        'A tour name must have greater or equal than 10 characters',
+      ],
+    },
+    slug: {
+      type: String,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration.'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size.'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price.'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        message: 'Discount price of ({VALUE}) should be below regular price',
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description.'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have an image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  slug: {
-    type: String,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration.'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size.'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty.'],
-    enum: {
-      values: ['easy', 'medium', 'difficult'],
-      message: 'Difficulty is either: easy, medium, difficult'
-    }
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price.'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description.']
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have an image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date]
-}, {
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true },
-});
+);
 
 // Virtual property on model to avoid data duplication
-tourSchema.virtual('durationWeeks').get(function() {
+tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
 // function is used to get access to this keyword
-// document middleware: runs before .save and .create
-tourSchema.pre('save', function(next) {
+// document middleware: runs before .save and .create but not .update
+tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
