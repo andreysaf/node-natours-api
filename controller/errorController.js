c70dacc5a123ad7f3cb0d3640506988e232290ae
Andrey <a.so777@live.ca>
2020-07-03 19:09:04 -0700

    Added error handling in prod vs dev

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,8 +1,34 @@
+const sendErrorInDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    });
+}
+
+const sendErrorInProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        console.log(`Error: ${err}`);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong',
+        });
+    }
+}
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'fail';
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if (process.env.NODE_ENV==='development') {
+        sendErrorInDev(err, res);
+    } else if (process.env.NODE_ENV==='production') {
+        sendErrorInProd(err, res);
+    }
+    
 };
