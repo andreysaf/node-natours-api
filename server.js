d4271862fd679d1fe5483f9aa9679702c8f28d59
Andrey Safonov <a.so777@live.ca>
2020-07-11 17:13:04 -0700

    Implemented error handling

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('unhandledException', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled exception, shutting down...');
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true  }).then(con => {
   console.log('DB connection successful');
@@ -9,7 +15,15 @@ mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndMo
 
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server listening on port: ${process.env.PORT}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection, shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
