538c52a3ce34f96ef32f9ff42135b9df16a8765f
Andrey <a.so777@live.ca>
2020-07-19 14:54:03 -0700

    Added JWT implementation for token creation

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,28 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../model/userModel');
-const catchAsync = require("../utils/catchAsync");
+const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    })
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign(
+    {
+      id: newUser._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
 });
