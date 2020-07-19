0fcbcca969fc5d5f6d5cd893c9ff523ee229be85
Andrey <a.so777@live.ca>
2020-07-19 11:48:04 -0700

    Added bcryptjs to hash passwords

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -35,6 +36,17 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords do not match'
     }
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined; // do not want to persist this field and only keep one 
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
