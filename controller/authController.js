89c1f345582366f5178407cc0307766ff0bfe73b
Andrey <a.so777@live.ca>
2020-08-23 09:49:01 -0700

    Implemented route restriction

--- controller/authController.js
+++ controller/authController.js
@@ -20,6 +20,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role
   });
 
   const token = signToken(newUser._id);
@@ -83,3 +84,14 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = foundUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    //roles ['admin', 'lead-guide']
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError('You do not have permission to perform this action', 403));
+    }
+
+    next();
+  }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { aliasTopTours, getTourStats, getMonthlyPlan, createTour, getAllTours, getTour, updateTour, deleteTour } = require('../controller/tourController');
-const { protect } = require('../controller/authController');
+const { protect, restrictTo } = require('../controller/authController');
 
 const router = express.Router();
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
