4aac01568f93cc04bbc887fdc02ed71a38e11e8e
Andrey <a.so777@live.ca>
2020-09-06 13:15:11 -0600

    Added http protection using helmet package

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
+
+// set security http headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
+// limit requests from the same IP
 const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour'
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser
+app.use(express.json({ limit: '10kb' }));
+
+// serving static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // runs for get, post, update and so on
 app.all('*', (req, res, next) => {
-    next(new AppError(`Cannot resolve the ${req.originalUrl} on this server.`, 404));
+  next(
+    new AppError(`Cannot resolve the ${req.originalUrl} on this server.`, 404),
+  );
 });
 
 // error handling middleware
