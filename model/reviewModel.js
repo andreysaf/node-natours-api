712830629387fb62cbfdd95408804a9bea6aae45
Andrey <a.so777@live.ca>
2020-09-07 10:59:34 -0600

    Implemented review model

---
+++ model/reviewModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review must have a body.'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    user: { type: mongoose.Schema.ObjectId, ref: 'User', required: [true, 'Review must belong to a user'], },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
