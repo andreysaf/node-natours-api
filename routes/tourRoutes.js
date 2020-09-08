6f367d5a2f08bc007a109dbdcb513a532a1da45f
Andrey <a.so777@live.ca>
2020-09-08 11:17:15 -0600

    Implemented nested routes for tours and reviews

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const { aliasTopTours, getTourStats, getMonthlyPlan, createTour, getAllTours, getTour, updateTour, deleteTour } = require('../controller/tourController');
+const {
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
+  createTour,
+  getAllTours,
+  getTour,
+  updateTour,
+  deleteTour,
+} = require('../controller/tourController');
 const { protect, restrictTo } = require('../controller/authController');
+const { createReview } = require('../controller/reviewController');
 
 const router = express.Router();
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+router
+  .route('/:tourId/reviews')
+  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
