5863c67a42daed00b658e47958830ab7073cf44c
Andrey <a.so777@live.ca>
2020-09-10 17:50:35 -0600

    Adding unique index for tour and user for a review

--- model/reviewModel.js
+++ model/reviewModel.js
@@ -35,6 +35,8 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
@@ -68,7 +70,6 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       ratingsAverage: 4.5,
     });
   }
-  
 };
 
 reviewSchema.post('save', function () {
@@ -77,13 +78,15 @@ reviewSchema.post('save', function () {
 });
 
 // update the review average after update or delete
-reviewSchema.pre(/^findOneAnd/, async function(next) {
+reviewSchema.pre(/^findOneAnd/, async function (next) {
   this.currentReview = await this.findOne();
   next();
 });
 
-reviewSchema.post(/^findOneAnd/, async function() {
-  await this.currentReview.constructor.calcAverageRatings(this.currentReview.tour);
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.currentReview.constructor.calcAverageRatings(
+    this.currentReview.tour,
+  );
 });
 
 const Review = mongoose.model('Review', reviewSchema);
