8c60694638d59ee0b7726599ed8f5b960176a9d6
Andrey <a.so777@live.ca>
2020-09-10 09:52:18 -0600

    Added authorization and restrict for review routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -10,14 +10,15 @@ const {
 const { protect, restrictTo } = require('../controller/authController');
 
 const router = express.Router({ mergeParams: true });
+router.use(protect);
 router
   .route('/')
   .get(getAllReviews)
-  .post(protect, restrictTo('user'), setTourUserIds, createReview);
+  .post(restrictTo('user'), setTourUserIds, createReview);
 router
   .route('/:id')
   .get(getReview)
-  .patch(updateReview)
-  .delete(protect, deleteReview);
+  .patch(restrictTo('user', 'admin'), updateReview)
+  .delete(restrictTo('user', 'admin'), deleteReview);
 
 module.exports = router;
