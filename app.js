21df515996a1b888919fb22a1bb94d7d476efd32
Andrey <a.so777@live.ca>
2020-09-20 19:35:13 -0700

    Implented extends logic

--- app.js
+++ app.js
@@ -65,6 +65,16 @@ app.get('/', (req, res) => {
     user: 'Andrey'
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker'
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
