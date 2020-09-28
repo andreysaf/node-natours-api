d95a0d18bebf2b544b2433aa95e1337fc7a44051
Andrey <a.so777@live.ca>
2020-09-27 18:29:12 -0700

    Implemented overview page

--- controller/viewsController.js
+++ controller/viewsController.js
@@ -1,11 +1,20 @@
-exports.getOverview = (req, res) => {
-    res.status(200).render('overview', {
-      title: 'All Tours',
-    });
-}
+const Tour = require('../model/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // get tour data
+  const tours = await Tour.find();
+
+  // build template
+  // render the template and send it back
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
+  });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
   });
-}
+};
